ae7d6e2045dceafba4306fef064f517e10a56b84
204G1A05c4 <94512875+204G1A05c4@users.noreply.github.com>
2022-09-16 15:08:15 +0530

    updated

--- oslab/exp1/FCFS/FCFS.c
+++ oslab/exp1/FCFS/FCFS.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 main()
 {
-  printf("k.Vyshnavi"\n)
-    printf("204g1a05c4")
-    printf("16/09/2003")
+  printf("k.Vyshnavi"\n);
+    printf("204g1a05c4");
+    printf("16/09/2003");
+    print("time:3:00pm");
   } 
